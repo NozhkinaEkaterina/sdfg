38173087d65d11b13d04042de6a7beaf8ca9833b
NozhkinaEkaterina <nozhkinaecaterina@yandex.ru>
2022-06-16 18:38:08 +0500

    Тесты

---
+++ WpfApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class WpfApp1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int input = 5;
+            var calc = new Calc();
+            int result = calc.sgrt(input);
+            int expected = 25;
+            Assert.AreEqual(expected, result);
+
+        }
+        [TestMethod]
+        public  void TestMethod2()
+      {
+        int input = 3;
+        var calc = new Calc();
+        int result = calc.sgrt(input);
+        int expected = 9;
+        Assert.AreEqual(expected, result);
+      }
+    }
+
+    
+}
---
+++ WpfApp1/WpfApp1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Class1
+    {
+        public int  sgrt(int a)
+        {
+            return a * a;
+        }
+    }
+}
